ac8d9a3725d93760a2d4d3842d71fe93c6c9e622
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-04 21:15:37 +0100

    tests: sample proto file for unit tests
    add example proto file and include it's generation in CMakeLists file.

--- tests/nanopb_basic_tests.cc
+++ tests/nanopb_basic_tests.cc
@@ -1,2 +1,9 @@
 #include <gtest/gtest.h>
 #include <pb.h>
+#include "test.pb.h"
+
+TEST(build_test, build_and_link_test)
+{
+	IntegerContainer container = { 0 };
+	ASSERT_EQ(IntegerContainer_fields[0].tag, IntegerContainer_fields[0].tag);
+}
