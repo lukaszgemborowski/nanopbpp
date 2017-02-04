a34b5e7fdb4c7e626d68a9ba80643f6fc86dc76c
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-04 23:38:39 +0100

    tests: basic_encoder tests moved to separate file
    extraction of basic_decoder tests to separate file

---
+++ tests/basic_encoder_tests.cc
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+#include <nanopbpp/basic_encoder.h>
+#include <pb_decode.h>
+
+#include "test.pb.h"
+
+TEST(basic_encoder_tests, encoding_into_vector)
+{
+	std::vector<uint8_t> buffer(512);
+	IntegerContainer source = { 0 };
+	IntegerContainer destination = { 0 };
+
+	source.a = 1;
+	source.has_b = true;
+	source.b = 2;
+
+	auto encoder = nanopbpp::create_encoder(buffer.begin(), buffer.end());
+	encoder.encode(IntegerContainer_fields, &source);
+
+	auto istream = pb_istream_from_buffer(buffer.data(), encoder.size());
+	ASSERT_TRUE(pb_decode(&istream, IntegerContainer_fields, &destination));
+
+	ASSERT_EQ(source.a, destination.a);
+	ASSERT_EQ(source.has_b, destination.has_b);
+	ASSERT_EQ(source.b, destination.b);
+}
--- tests/nanopb_basic_tests.cc
+++ tests/nanopb_basic_tests.cc
@@ -1,5 +1,3 @@
-#include <vector>
-#include <nanopbpp/basic_encoder.h>
 #include <gtest/gtest.h>
 #include <pb.h>
 #include <pb_decode.h>
@@ -33,23 +31,4 @@ TEST(nanopb, encode_decode_basic_buffer)
 	ASSERT_EQ(source.b, destination.b);
 }
 
-TEST(nanopb, custom_encoder_with_vector)
-{
-	std::vector<uint8_t> buffer(512);
-	IntegerContainer source = { 0 };
-	IntegerContainer destination = { 0 };
 
-	source.a = 1;
-	source.has_b = true;
-	source.b = 2;
-
-	auto encoder = nanopbpp::create_encoder(buffer.begin(), buffer.end());
-	encoder.encode(IntegerContainer_fields, &source);
-
-	auto istream = pb_istream_from_buffer(buffer.data(), encoder.size());
-	ASSERT_TRUE(pb_decode(&istream, IntegerContainer_fields, &destination));
-
-	ASSERT_EQ(source.a, destination.a);
-	ASSERT_EQ(source.has_b, destination.has_b);
-	ASSERT_EQ(source.b, destination.b);
-}
