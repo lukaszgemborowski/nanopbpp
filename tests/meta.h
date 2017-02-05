1be6b3a2fb3fb78706e41332893dc8c808c1b0e4
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-05 14:17:36 +0100

    tests: use helpers to encode/decode message with extensions
    proof that functionality is working

--- tests/meta.h
+++ tests/meta.h
@@ -4,5 +4,9 @@
 #include "test.pb.h"
 
 static std::tuple<
-	nanopbpp::message_metadata<IntegerContainer>
-> messages_metadata = std::make_tuple(IntegerContainer_fields);
+	nanopbpp::message_metadata<IntegerContainer>,
+	nanopbpp::message_metadata<Extendable>
+> messages_metadata = std::make_tuple(
+	IntegerContainer_fields,
+	Extendable_fields
+);
--- tests/uncategorized_tests.cc
+++ tests/uncategorized_tests.cc
@@ -46,3 +46,39 @@ TEST(uncategorized_tests, encode_decode_with_meta_helpers)
 	ASSERT_EQ(src.has_b, dst.has_b);
 	ASSERT_EQ(src.b, dst.b);
 }
+
+TEST(uncategorized_tests, encode_decode_with_extension_and_helpers)
+{
+	std::vector<uint8_t> buffer(256);
+	IntegerContainer source_field = { 0 };
+	IntegerContainer destination_field = { 0 };
+
+	source_field.a = 2;
+	source_field.has_b = true;
+	source_field.b = 3;
+
+	Extendable source = { 0 };
+	Extendable destination = { 0 };
+
+	pb_extension_t ext = { 0 };
+	ext.type = &field_a;
+	ext.dest = &source_field;
+	ext.next = nullptr;
+	ext.found = false;
+
+	source.extensions = &ext;
+
+	ASSERT_TRUE(nanopbpp::create_encoder(buffer.begin(), buffer.end(), messages_metadata).encode(source));
+
+	ext.type = &field_a;
+	ext.next = nullptr;
+	ext.found = false;
+	ext.dest = &destination_field;
+	destination.extensions = &ext;
+
+	ASSERT_TRUE(nanopbpp::create_decoder(buffer.begin(), buffer.end(), messages_metadata).decode(destination));
+
+	ASSERT_EQ(source_field.a, destination_field.a);
+	ASSERT_EQ(source_field.has_b, destination_field.has_b);
+	ASSERT_EQ(source_field.b, destination_field.b);
+}
