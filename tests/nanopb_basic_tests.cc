8947f4df857d085fe84346daf3b1064801dfb7ba
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-09 23:49:19 +0100

    tests: nanopb test decoding with callback
    + missing definition in proto file for previous tests

--- tests/nanopb_basic_tests.cc
+++ tests/nanopb_basic_tests.cc
@@ -48,7 +48,6 @@ TEST(nanopb, encode_decode_empty_message_in_container)
 	ASSERT_TRUE(pb_decode(&istream, Container_fields, &destination));
 }
 
-
 TEST(nanopb, encode_decode_with_extension)
 {
 	uint8_t buffer[256];
@@ -85,19 +84,6 @@ TEST(nanopb, encode_decode_with_extension)
 	ASSERT_EQ(source_field.a, destination_field.a);
 	ASSERT_EQ(source_field.has_b, destination_field.has_b);
 	ASSERT_EQ(source_field.b, destination_field.b);
-}
-
-struct context {
-	context(bool &value) : value (value) {}
-
-	bool &value;
-};
-
-bool nanopb_tests_decode_1(pb_istream_t *stream, pb_extension_t *extension, uint32_t tag, pb_wire_type_t wire_type)
-{
-	context *ctx = (context *)extension->dest;
-	ctx->value = true;
-	return true;
 }
 
 TEST(nanopb, BUG_encode_decode_extension)
@@ -159,4 +145,54 @@ TEST(nanopb, decode_multiple_extensions)
 	ASSERT_EQ(source_field_a.a, destination_field_a.a);
 	ASSERT_EQ(source_field_a.b, destination_field_a.b);
 	ASSERT_EQ(source_field_b.c, destination_field_b.c);
+}
+
+bool nanopb_tests_decode_1(pb_istream_t *stream, pb_extension_t *extension, uint32_t tag, pb_wire_type_t wire_type)
+{
+	int *call_count = (int *)extension->dest;
+	*call_count = true;
+	return true;
+}
+
+TEST(nanopb, decode_multiple_extensions_with_simple_callback)
+{
+	uint8_t buffer[256];
+	IntegerContainer source_field_a = { 0 };
+	FloatContainer source_field_b = { 0 };
+	Extendable source = { 0 }, destination = { 0 };
+
+	source_field_a.a = 10;
+	source_field_a.has_b = true;
+	source_field_a.b = 20;
+	source_field_b.c = 30.4f;
+
+	pb_extension_t source_ext_a = { 0 }, source_ext_b = { 0 };
+	source_ext_a.type = &field_a;
+	source_ext_a.dest = &source_field_a;
+	source_ext_a.next = &source_ext_b;
+	source_ext_b.type = &field_b;
+	source_ext_b.dest = &source_field_b;
+
+	source.extensions = &source_ext_a;
+
+	auto ostream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+	ASSERT_TRUE(pb_encode(&ostream, Extendable_fields, &source));
+
+	int call_count = 0;
+	pb_field_t type_a = { 0 };
+	pb_extension_type_t extension_type_a = { 0 };
+	pb_extension_t destination_ext_a = { 0 };
+
+	extension_type_a.decode = &nanopb_tests_decode_1;
+	extension_type_a.arg = &type_a;
+
+	destination_ext_a.type = &extension_type_a;
+	destination_ext_a.dest = &call_count;
+
+	destination.extensions = &destination_ext_a;
+
+	auto istream = pb_istream_from_buffer(buffer, ostream.bytes_written);
+	ASSERT_TRUE(pb_decode(&istream, Extendable_fields, &destination));
+
+	ASSERT_EQ(1, call_count);
 }
