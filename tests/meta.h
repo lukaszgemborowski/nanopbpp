5467d4b0aec5c394b099d44e7d506c48e0d11432
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-11 17:44:56 +0100

    tests: properly define meta for Extendable field

--- tests/meta.h
+++ tests/meta.h
@@ -3,11 +3,17 @@
 #include <nanopbpp/metadata.h>
 #include "test.pb.h"
 
+
 static nanopbpp::messages_metadata_set<
 	nanopbpp::message_metadata<IntegerContainer>,
-	nanopbpp::message_metadata<Extendable>
+	nanopbpp::message_metadata<Extendable,
+		nanopbpp::extensions_metadata_set<
+			nanopbpp::extension_metadata<field_a_tag, IntegerContainer>,
+			nanopbpp::extension_metadata<field_b_tag, FloatContainer>
+		>
+	>
 > messages_metadata(
-	IntegerContainer_fields,
-	Extendable_fields
+	{ IntegerContainer_fields },
+	{ Extendable_fields, {field_a, field_b} }
 );
 
