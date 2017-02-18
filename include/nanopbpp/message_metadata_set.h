5b971e7be1f4743ff471996c86bcd13ffd13ce0e
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-18 18:48:21 +0100

    message_metadata_set: extract to own header

---
+++ include/nanopbpp/message_metadata_set.h
@@ -0,0 +1,49 @@
+#pragma once
+
+namespace nanopbpp
+{
+
+template<typename... M>
+class message_metadata_set
+{
+	template<typename T, size_t N, typename... Args>
+	struct get_by_type_impl
+	{
+		static constexpr auto value = N;
+	};
+
+	template<typename T, size_t N, typename... Args>
+	struct get_by_type_impl<T, N,
+		message_metadata<T,
+				typename std::tuple_element<N, std::tuple<M...>>::type::extensions_set_t
+			>,
+		Args...>
+	{
+		static constexpr auto value = N;
+	};
+
+	template<typename T, size_t N, typename U, typename... Args>
+	struct get_by_type_impl<T, N, U, Args...>
+	{
+		static constexpr auto value = get_by_type_impl<T, N + 1, Args...>::value;
+	};
+
+public:
+	template<typename T, typename E = typename std::tuple_element<get_by_type_impl<T, 0, M...>::value, std::tuple<M...>>::type::extensions_set_t>
+	message_metadata<T, E>& get_by_meta_type()
+	{
+		std::get<
+			get_by_type_impl<T, 0, M...>::value>(messages);
+	}
+
+	template<typename T>
+	const pb_field_t *fields()
+	{
+		return get_by_meta_type<T>().fields;
+	}
+
+	message_metadata_set(M... messages) : messages(std::make_tuple(messages...)) {}
+	std::tuple<M...> messages;
+};
+
+}
