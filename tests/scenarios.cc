0d159ce105ecc4c84a7a55a6e747dba3f8f66b64
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-18 21:29:14 +0100

    tests: user scenarios
    two more encode/decode test cases showing ability to operate either on
    std::array or standard C_like array

--- tests/scenarios.cc
+++ tests/scenarios.cc
@@ -31,3 +31,53 @@ TEST(scenarios, encode_and_decode_simple_message_with_static_buffer)
 	ASSERT_EQ(source.b, destination.b);
 	ASSERT_EQ(source.has_b, destination.has_b);
 }
+
+TEST(scenarios, encode_and_decode_simple_message_with_array_buffer)
+{
+	using namespace nanopbpp;
+
+	IntegerContainer source = {0}, destination = {0};
+
+	// prepare data to be encoded in protobuf, basically
+	// you need to fill standard nanopb generated structure
+	source.a = 10;
+	source.has_b = true;
+	source.b = 20;
+
+	std::array<uint8_t, 64> buffer = {0};
+
+	// encode message to buffer
+	ASSERT_TRUE(encode(messages_metadata, buffer.begin(), buffer.end(), source));
+
+	// decodemessage from buffer
+	ASSERT_TRUE(decode(messages_metadata, buffer.begin(), buffer.end(), destination));
+
+	ASSERT_EQ(source.a, destination.a);
+	ASSERT_EQ(source.b, destination.b);
+	ASSERT_EQ(source.has_b, destination.has_b);
+}
+
+TEST(scenarios, encode_and_decode_simple_message_with_C_array)
+{
+	using namespace nanopbpp;
+
+	IntegerContainer source = {0}, destination = {0};
+
+	// prepare data to be encoded in protobuf, basically
+	// you need to fill standard nanopb generated structure
+	source.a = 10;
+	source.has_b = true;
+	source.b = 20;
+
+	uint8_t buffer[64] = {0};
+
+	// encode message to buffer
+	ASSERT_TRUE(encode(messages_metadata, buffer, buffer + sizeof(buffer), source));
+
+	// decodemessage from buffer
+	ASSERT_TRUE(decode(messages_metadata, buffer, buffer + sizeof(buffer), destination));
+
+	ASSERT_EQ(source.a, destination.a);
+	ASSERT_EQ(source.b, destination.b);
+	ASSERT_EQ(source.has_b, destination.has_b);
+}
