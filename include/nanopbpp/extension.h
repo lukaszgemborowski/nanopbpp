0dd299bc93b6a347ab68de81dc0b2479af7daa4d
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-18 16:33:39 +0100

    base_extensions: extract to own file

--- include/nanopbpp/extension.h
+++ include/nanopbpp/extension.h
@@ -2,44 +2,10 @@
 #include <type_traits>
 #include <functional>
 #include <pb.h>
+#include "base_extension.h"
 
 namespace nanopbpp
 {
-
-template<size_t TAG>
-class base_extension
-{
-public:
-	static constexpr size_t EXTENSION_TAG = TAG;
-
-	base_extension(const pb_extension_type_t &original) :
-		definition (original)
-	{
-	}
-
-	template<typename U>
-	void attach_to_message(pb_extension_t &extension, U &message) const
-	{
-		extension.type = &definition;
-
-		if (message.extensions) {
-			extension.next = message.extensions;
-			message.extensions = &extension;
-		} else {
-			extension.next = nullptr;
-			message.extensions = &extension;
-		}
-	}
-
-	template<typename T>
-	void attach_to_storage(pb_extension_t &extension, T &storage) const
-	{
-		extension.dest = &storage;
-	}
-
-protected:
-	pb_extension_type_t definition;
-};
 
 template<size_t TAG, typename T>
 class extension : public base_extension<TAG>
