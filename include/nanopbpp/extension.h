a29146db5871178777eef2dfb729a969d3e825a7
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-02-19 15:55:52 +0100

    base_extension & callback_extension: removed
    functionality of callback will be rewritten

--- include/nanopbpp/extension.h
+++ include/nanopbpp/extension.h
@@ -2,30 +2,40 @@
 #include <type_traits>
 #include <functional>
 #include <pb.h>
-#include "base_extension.h"
 
 namespace nanopbpp
 {
 
 template<size_t TAG, typename T>
-class extension : public base_extension<TAG>
+class extension
 {
 public:
+	static constexpr auto EXTENSION_TAG = TAG;
+
 	extension(const pb_extension_type_t &original) :
-		base_extension<TAG>(original),
+		definition(original),
 		pb_extension {0}
 	{
 	}
 
 	void attach_to_storage(T &storage)
 	{
-		base_extension<TAG>::attach_to_storage(pb_extension, storage);
+		pb_extension.dest = &storage;
 	}
 
 	template<typename M>
 	void attach_to_message(M &message)
 	{
-		base_extension<TAG>::attach_to_message(pb_extension, message);
+		pb_extension.type = &definition;
+
+		if (message.extensions) {
+			pb_extension.next = message.extensions;
+			message.extensions = &pb_extension;
+		} else {
+			pb_extension.next = nullptr;
+			message.extensions = &pb_extension;
+		}
+
 	}
 
 	template<typename M>
@@ -43,52 +53,12 @@ public:
 
 	bool has_storage_attached() const
 	{
-		base_extension<TAG>::has_storage_attached(pb_extension);
+		return pb_extension.dest != nullptr;
 	}
 
 private:
+	pb_extension_type_t definition;
 	pb_extension_t pb_extension;
-};
-
-template<size_t TAG>
-class callback_extension : public base_extension<TAG>
-{
-public:
-	callback_extension(const pb_extension_type_t &extension) :
-		base_extension<TAG> (extension),
-		callback(),
-		extension {0},
-		fake {0} // this fake is importat, it needs to be 0-ed out (especially tag field)
-	{
-		this->definition.decode = &callback_extension<TAG>::decode;
-		this->definition.encode = nullptr;
-		this->definition.arg = &fake;
-	}
-
-	template<typename U, typename F>
-	void attach(U &message, const F &func)
-	{
-		this->attach_to_message(extension, message);
-		extension.type = &this->definition;
-		extension.dest = this;
-		callback = func;
-	}
-
-private:
-	static bool decode(pb_istream_t *stream, pb_extension_t *extension, uint32_t tag, pb_wire_type_t wire_type)
-	{
-		if (tag != TAG)
-			return true;
-
-		callback_extension<TAG> *self = reinterpret_cast<callback_extension<TAG> *>(extension->dest);
-		self->callback();
-		return true;
-	}
-
-private:
-	std::function<void ()> callback;
-	pb_extension_t extension;
-	pb_field_t fake;
 };
 
 }
